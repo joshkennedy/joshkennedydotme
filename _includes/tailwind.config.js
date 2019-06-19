f254349d0a016df034b0a077756eb5f3f081e007
Josh Kennedy <jshknndy@gmail.com>
2019-06-18 20:34:14 -0500

    Rebirth

---
+++ _includes/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    extend: {},
+    screens: {
+      sm: '600px',
+      md: '768px',
+      lg: '1024px',
+    },
+    fontFamily: {
+      mono: ['Roboto Mono', 'monospace'],
+      sans: ['Cabin', 'sans-serif'],
+    },
+  },
+  variants: {},
+  plugins: []
+}
